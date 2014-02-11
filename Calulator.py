0359999b86161cb2021677a29a6a62f6c68fad72
darthvader01 <ollie@mclane.cc>
2014-02-10 20:12:59 -0700

    calculator

---
+++ Calulator.py
@@ -0,0 +1,20 @@
+num1=input("Please type the first number: ")
+num2=input("Please type the second number: ")
+num1=int(num1)
+num2=int(num2)
+numSum = num1 + num2
+print(numSum)
+print(" This is the Sum")
+numMinus = num1 - num2
+print (numMinus)
+print (" This is the Minus")
+numMultip = num1 * num2
+print (numMultip)
+print("This is the Multiplacation")
+numDiv= num1 / num2
+print (numDiv)
+print ("This is the division")
+
+
+
+
